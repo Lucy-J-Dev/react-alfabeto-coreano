bcf61f239c1d43f1efce72004291dd24ff3cfb68
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2025-05-26 19:14:34 -0500

    Cree estados y use el useEffect para traer el resumen del idioma y se hizo visible la data que se consulto del api

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,8 @@ import CardContent from "../components/card/CardContent";
 import Card from "../components/card/Card";
 import CardHeader from "../components/card/CardHeader";
 import CardTitle from "../components/card/CardTitle";
+import { useEffect, useState } from "react";
+import { getSummary } from "../services/api";
 
 type DashboardItem = {
   title: string;
@@ -10,52 +12,72 @@ type DashboardItem = {
   desc: string;
 };
 
-const dashboardData: DashboardItem[] = [
-  {
-    title: "Total de caracteres",
-    count: 40,
-    desc: "Cantidad total de caracteres que hay registrados en la aplicación.",
-  },
-  {
-    title: "Vocales",
-    count: 21,
-    desc: "10 vocales basicas y 11 vocales compuestas.",
-  },
-  {
-    title: "Consonantes",
-    count: 19,
-    desc: "14 consonantes basicas y 5 consonantes dobles.",
-  },
-];
+type Summary = {
+  title: string;
+  summary: string;
+  content: Content[];
+};
+
+type Content = {
+  title: string;
+  count: number;
+  description: string;
+};
 
 const Dashboard = () => {
+  const [dataSummary, setDataSummary] = useState<Summary>({ title: "", summary: "", content: [] });
+  const [loading, setLoading] = useState(false);
+  const [messageError, setMessageError] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    const handleCallGetSumaryApi = async () => {
+      try {
+        const data = await getSummary();
+        setDataSummary(data);
+        console.log(data);
+      } catch (error) {
+        setMessageError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    handleCallGetSumaryApi();
+  }, []);
+
   return (
     <div className="bg-white flex flex-col gap-6 rounded-3xl shadow-md hover:shadow-xl transition-shadow duration-300">
-      {/* Encabezado */}
-      <div className="flex flex-col gap-2 p-6 ">
-        <h2 className="text-3xl font-bold tracking-tight">Alfabeto Coreano</h2>
-        <p className="text-muted-foreground">... colocar algo ...</p>
-      </div>
+      {loading ? (
+        <p>Cargando</p>
+      ) : (
+        <div>
+          {/* Encabezado */}
+          <div className="flex flex-col gap-2 p-6 ">
+            <h2 className="text-3xl font-bold tracking-tight">{dataSummary.title}</h2>
+            <p className="text-muted-foreground">{dataSummary.summary}</p>
+          </div>
 
-      {/* Contenido */}
-      <div className="flex flex-col p-6 gap-4">
-        {dashboardData.map((item, index) => (
-          <Card key={index}>
-            <CardHeader>
-              <CardTitle>{item.title}</CardTitle>
-              <div className="bg-primary p-1.5 cursor-pointer rounded-md shadow-md hover:shadow-lg active:scale-95 transition-all duration-300">
-                <CiMenuKebab className="h-6 w-6 text-accent" />
-              </div>
-            </CardHeader>
-            <CardContent>
-              <div>
-                <p className="text-4xl font-semibold text-accent">{item.count}</p>
-              </div>
-              <p className="text-base text-muted-foreground">{item.desc}</p>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+          {/* Contenido */}
+          <div className="flex flex-col p-6 gap-4">
+            {dataSummary.content.map((item, index) => (
+              <Card key={index}>
+                <CardHeader>
+                  <CardTitle>{item.title}</CardTitle>
+                  <div className="bg-primary p-1.5 cursor-pointer rounded-md shadow-md hover:shadow-lg active:scale-95 transition-all duration-300">
+                    <CiMenuKebab className="h-6 w-6 text-accent" />
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <div>
+                    <p className="text-4xl font-semibold text-accent">{item.count}</p>
+                  </div>
+                  <p className="text-base text-muted-foreground">{item.description}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
